9b51caf1b0fb899b3f19dddf0a98c9ba881f8cb6
skycs97 <skycs97@gmail.com>
2025-07-02 16:11:08 +0900

    [feature] implementation length checker

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -1,5 +1,27 @@
 #include <string>
 
+using std::string;
+
 class SimilarityChecker {
 public:
+	int  checkLength(const string& a, const string& b) {
+		if (a.length() == b.length()) {
+			return 60.;
+		}
+
+		double gap = 0;
+		if (a.length() > b.length()) {
+			gap = a.length() - b.length();
+		}
+		else {
+			gap = b.length() - a.length();
+		}
+
+		if (a.length() > b.length()) {
+			return floor((1.f - (gap / b.length())) * 60);
+		}
+		else {
+			return floor((1.f - (gap / a.length())) * 60);
+		}
+	}
 };
--- SimilarityChecker/similarity_checker_test.cpp
+++ SimilarityChecker/similarity_checker_test.cpp
@@ -1,8 +1,51 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.cpp"
+#include <iostream>
+#include <cstdio>
 
-TEST(TS, TC1) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityCheckerTest, LengthScore60) {
+	SimilarityChecker checker;
+
+	string a = "AAA";
+	string b = "BBB";
+	
+	int expected = 60;
+	int actual = checker.checkLength(a, b);
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SimilarityCheckerTest, LengthScore0) {
+	SimilarityChecker checker;
+
+	string a = "A";
+	string b = "BB";
+
+	int expected = 0;
+	int actual = checker.checkLength(a, b);
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SimilarityCheckerTest, LengthScore20) {
+	SimilarityChecker checker;
+
+	string a = "AAABB";
+	string b = "BAA";
+
+	int expected = 20.;
+	int actual = checker.checkLength(a, b);
+
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SimilarityCheckerTest, LengthScore30) {
+	SimilarityChecker checker;
+
+	string a = "AA";
+	string b = "AAE";
+
+	int expected = 30;
+	int actual = checker.checkLength(a, b);
+	EXPECT_EQ(expected, actual);
 }
 
 int main(void) {
